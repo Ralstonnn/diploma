36d731c8918def95ff356d18ceb0ef6d101165f3
Ralstonnn <zigthehedge2002@gmail.com>
2022-05-29 01:30:44 +0500

    comit before reinstall

--- src/components/Trainings/SpellCheck/SpellCheck.jsx
+++ src/components/Trainings/SpellCheck/SpellCheck.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { LoadingAnimation } from "../../LoadingAnimation/LoadingAnimation";
 import { SpellCheckCard } from "./parts/TrainingCard";
@@ -8,6 +8,18 @@ export function SpellCheck() {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(true);
   const [index, setIndex] = useState(0);
+  const [data, setData] = useState([]);
+
+  const test = (i) => {
+    return data[index].lettest[i].value;
+  };
+
+  const inputOnChange = (inputIndex, value) => {
+    let tempArr = data;
+    tempArr[index].letters[inputIndex].value = value;
+    console.log(tempArr);
+    setData(tempArr);
+  };
 
   useEffect(() => {
     fetch("/api/spell-check")
@@ -18,6 +30,20 @@ export function SpellCheck() {
           return navigate("/training");
         }
 
+        setData(
+          res.map((item) => {
+            item.letters = item.word.split("").map((letter) => {
+              letter = {
+                rigthLetter: letter,
+                isAnsweredRight: false,
+                value: "",
+              };
+              return letter;
+            });
+
+            return item;
+          })
+        );
         setIsLoading(false);
       });
   }, [navigate]);
@@ -26,7 +52,18 @@ export function SpellCheck() {
 
   return (
     <div className="spell-check-container flex-item flex flex-j-center">
-      <SpellCheckCard />
+      {/* <SpellCheckCard /> */}
+      {/* <button onClick={test}> test</button> */}
+      {/* <br /> */}
+      {console.log("redrawn")}
+      {data[0].letters.map((letter, i) => (
+        <input
+          value={data[0].letters[i].value}
+          onChange={(e) => inputOnChange(i, e.target.value)}
+          key={i}
+          style={{ border: "1px solid black", marginLeft: "20px" }}
+        />
+      ))}
     </div>
   );
 }
